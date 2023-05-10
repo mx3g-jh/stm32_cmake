b2dc9df2c7808a7b7bdfb98bb69cc261d7b4eb90
mx3g-jh <mx3g2728692769@gmail.com>
2023-05-10 09:50:58 +0800

    add h7 template

--- src/stm32proj/cubemx/Core/Src/main.c
+++ src/stm32proj/cubemx/Core/Src/main.c
@@ -48,7 +48,6 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-static void MPU_Config(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -67,9 +66,6 @@ int main(void)
   /* USER CODE BEGIN 1 */
 
   /* USER CODE END 1 */
-
-  /* MPU Configuration--------------------------------------------------------*/
-  MPU_Config();
 
   /* MCU Configuration--------------------------------------------------------*/
 
@@ -98,7 +94,9 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+    HAL_Delay(50);
+    HAL_GPIO_TogglePin(BLUE_LED_GPIO_Port,BLUE_LED_Pin);
+    HAL_Delay(50);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -166,35 +164,6 @@ void SystemClock_Config(void)
 /* USER CODE BEGIN 4 */
 
 /* USER CODE END 4 */
-
-/* MPU Configuration */
-
-void MPU_Config(void)
-{
-  MPU_Region_InitTypeDef MPU_InitStruct = {0};
-
-  /* Disables the MPU */
-  HAL_MPU_Disable();
-
-  /** Initializes and configures the Region and the memory to be protected
-  */
-  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
-  MPU_InitStruct.Number = MPU_REGION_NUMBER0;
-  MPU_InitStruct.BaseAddress = 0x0;
-  MPU_InitStruct.Size = MPU_REGION_SIZE_4GB;
-  MPU_InitStruct.SubRegionDisable = 0x87;
-  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
-  MPU_InitStruct.AccessPermission = MPU_REGION_NO_ACCESS;
-  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
-  MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
-  MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
-  MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
-
-  HAL_MPU_ConfigRegion(&MPU_InitStruct);
-  /* Enables the MPU */
-  HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
-
-}
 
 /**
   * @brief  This function is executed in case of error occurrence.
--- src/stm32proj/target-def.cmake
+++ src/stm32proj/target-def.cmake
@@ -1,16 +1,16 @@
 # Target-specific flags
-set(MCU_ARCH ARMCM3)
-set(MCU_FAMILY STM32F103xB)
-set(MCU_NAME STM32F103C8)
+set(MCU_ARCH ARMCM7)
+set(MCU_FAMILY STM32H743xx)
+set(MCU_NAME STM32H743VI)
 
 # You can find the following values from ./cubemx/Makefile
-set(CPU "-mcpu=cortex-m3")
-set(FPU "")
-set(FLOAT_ABI "")
+set(CPU "-mcpu=cortex-m7")
+set(FPU "-mfpu=fpv5-d16")
+set(FLOAT_ABI "-mfloat-abi=hard")
 
-set(LINKER_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/cubemx/STM32F103C8Tx_FLASH.ld)
+set(LINKER_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/cubemx/STM32H743VITx_FLASH.ld)
 
 option(USE_LL_LIB "Enable LL library" ON)
-option(USE_HAL_LIB "Enable HAL library" OFF)
+option(USE_HAL_LIB "Enable HAL library" ON)
 
 option(USE_SYSTEM_VIEW "Enable Segger SystemView library" OFF)
